2d9c236b142fb4b105f06ab8f0c6e9689e7c39cc
caojinglang@teiron.com <caojinglang@teiron.com>
2014-04-29 11:58:09 +0800

    test

--- Hello/src/com/example/hello/MainActivity.java
+++ Hello/src/com/example/hello/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toast.makeText(this, "Hello", 0);
+		Toast.makeText(this, "yyyyy", 0);
         //AAAA
         //BBBB
 	}
